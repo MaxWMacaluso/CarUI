f5a004c43265926c4a2d26fb4f7f66c032ba0c63
Sam Herwif <Sahe2032@colorado.edu>
2020-04-13 15:04:02 -0600

    not sure

---
+++ Mechamarkers/Website_interface/Server_local/Readme 6.md
@@ -0,0 +1,60 @@
+# Mechamarkers Boilerplate for Javascript
+This project includes all of the code you will need to get started running a Mechamarkers app on your local server.
+
+## Setup
+- Install Node.js from [here](https://nodejs.org/en/)
+- Then download this repo (if you do it as a zip extract it)
+- Open a terminal and navigate into the directory of this project
+- Run `npm install` in the terminal
+- Then run `npm run build` to compile the library
+
+## Running the server
+- Run `npm start`. This will start a local server and keep it running until you exit the process with Ctrl-C or close the terminal window.
+- Go to `localhost:8080/index.html` in a browser
+
+## Editing the Code
+To just make a simple app all of the code you will need to edit is in the index.html file. You will probably only need to edit the callback for `window.onload` for initialization, and the body of the `update` loop for your main loop.
+
+## Using the Library
+When using this bootstrap, the library is exposed under the hood already. The library must be initialized and manually updated which the starter code already does for you. Here are the important functions to know. The library is exposed as a member of the `window` object as `window.Mechamarkers`.
+
+### Simple 2D Vectors
+Many objects in this library have 2D vector objects associated with them, they are a simple x and y value and look like this `{ x: value, y : value }` and are what we are referring to when we say something is a vector type.
+
+### Markers
+`Mechamarkers.getMarker(markerID)` will return a marker object that has the following properties.
+
+| Property | Type | Description |
+| --- | --- | --- |
+| **id** | `number` | integer id of the marker |
+| **present** | `boolean` | is true when the marker is currently visible by the camera |
+| **center** | `vector` | the center point of the marker in camera space |
+| **corner** | `vector` | the top left corner of the marker in camera space, used to calculate orientation |
+| **allCorners** | `vector` | the positions of all marker corners in camera space |
+
+### Input Groups
+`Mechamarkers.getGroup(groupName)` will return an input group object that has the following properties.These will only appear if you have created and saved a group config with the **Input Generator** in the desktop app. You may only have one config saved at a time. Input groups have the following properties and methods.
+
+| Property | Type | Description |
+| --- | --- | --- |
+| **name** | `string` | name given to the group in the config |
+| **angle** | `number` | rotation of the entire group in radians |
+| **pos** | `vector` | position of the center point of the group's anchor |
+| **anchor** | `Marker` | identifying marker of the group set in the config |
+| **inputs** | `input[]` | array of all of the inputs in this group |
+
+| Method Name | Parameters | Return Type | Description |
+| --- | --- | --- | --- |
+| **getInput** | `string: inputName` | `input` | returns and an input object of the given name |
+| **getInputByID** | `number: inputID` | `input` | returns and input object with the given id in this group's inputs array |
+| **isPresent** | `N\A` | `boolean` | returns true when a group's anchor is detected |
+
+### Inputs
+Inputs can only be accessed through a groups `getInput` function described above. Below are their properties.
+
+| Property | Type | Description |
+| --- | --- | --- |
+| **name** | `string` | name given to the input in the config |
+| **type** | `string` | type of input can be *BUTTON, TOGGLE, SLIDER, or KNOB* |
+| **val** | `number` | current value of the input based on the calculation method for each type |
+| **actor** | `Marker` | marker tied to this input |
---
+++ Mechamarkers/Website_interface/Server_local/src/Mechamarkers/InputGroup 7.js
@@ -0,0 +1,124 @@
+import { inv } from 'mathjs';
+import Button from './InputClasses/Button';
+import Toggle from './InputClasses/Toggle';
+import Knob from './InputClasses/Knob';
+import Slider from './InputClasses/Slider';
+import * as Vec2 from './Utils/Vec2';
+
+import { calDistortionMatrices, matrixTransform } from './Utils/Distortion';
+const config = {};
+
+const { vecSub, vecRot, vecScale, vecAngleBetween, vecEMA, vecMag } = Vec2;
+
+// These need to be made constants
+const CORNER_ANGLE = -3*Math.PI/4;
+const xaxis = {x:1, y:0};
+const yaxis = {x:0, y:1};
+const angleRefAxis = xaxis;
+
+class InputGroup {
+  constructor(markerData, config) {
+    this.name = config.name;
+    if (config.anchorID !== '') {
+      this.anchor = markerData[config.anchorID];
+      this.anchor.timeout = config.detectWindow;
+      this.anchor.inuse = true;
+    }
+    this.inputs = config.inputs.map((i) => {
+      switch(i.type) {
+        case 'BUTTON':
+          return new Button(markerData, i);
+        case 'TOGGLE':
+          return new Toggle(markerData, i);
+        case 'KNOB':
+          return new Knob(markerData, i);
+        case 'SLIDER':
+          return new Slider(markerData, i);
+        default:
+          break;
+      }   
+    });
+
+    this.boundingBox = { //set with calBoundingBox()
+      x: -1, 
+      y: -1,
+      w: -1,
+      h: -1,
+    };
+    this.angle = 0;
+    this.pos = {x:0, y:0};
+
+    // Undistortion Matrix stuff
+    this.matrixRect2Quad;
+    this.matrixQuad2Rect;
+
+    this.markerSize = config.markerSize;
+    this.markerCorners = [
+      { x: -config.markerSize/2, y: -config.markerSize/2 },
+      { x: config.markerSize/2, y: -config.markerSize/2 },
+      { x: config.markerSize/2, y: config.markerSize/2 },
+      { x: -config.markerSize/2, y: config.markerSize/2 }
+    ];
+
+    this.cornerAngleGroup = 1*Math.PI/4;
+    this.cornerAngleInput = -1*Math.PI/4;
+  }
+
+  getInput(inputName) {
+    // throw error when can't find input by name
+    return this.inputs.find(i => i.name == inputName);
+  }
+
+  getInputByID(id) {
+    return this.inputs[id];
+  }
+
+  isPresent() {
+    return this.anchor.present;
+  }
+
+  calBoundingBox(markerOffsetSize, pxpermm) {
+    if (!this.anchor) return;
+    let centerPts = this.inputs.map(i => {
+      if (!i || !i.actor) return { x: 0, y: 0 };
+      // Give each input class a get center point
+      return (vecRot(vecScale(xaxis, i.relativePosition.distance*pxpermm), i.relativePosition.angle - this.cornerAngleInput));
+    });
+    centerPts.push({x:0, y:0});
+
+    centerPts.sort((a, b) => (a.x - b.x));
+    const xmax = centerPts[centerPts.length-1].x;
+    const xmin = centerPts[0].x;
+    const xw = xmax - xmin;
+    
+    centerPts.sort((a, b) => (a.y - b.y));
+    const ymax = centerPts[centerPts.length-1].y;
+    const ymin = centerPts[0].y;
+    const yh = ymax - ymin;
+    
+    this.boundingBox.x = xmin - markerOffsetSize;
+    this.boundingBox.y = ymin - markerOffsetSize;
+    this.boundingBox.w = xw + markerOffsetSize*2;
+    this.boundingBox.h = yh + markerOffsetSize*2;
+  }
+
+  update() {
+    if (!this.anchor) return;
+    this.angle = -vecAngleBetween(vecSub(this.anchor.center, this.anchor.corner), angleRefAxis) - this.cornerAngleGroup;
+    this.pos = vecEMA(this.anchor.center, this.pos, 0.5);
+    if (this.anchor.present) {
+      this.matrixRect2Quad = calDistortionMatrices(
+        this.anchor.allCorners[0], this.anchor.allCorners[1], this.anchor.allCorners[2], this.anchor.allCorners[3],
+        this.markerCorners[0], this.markerCorners[1], this.markerCorners[2], this.markerCorners[3]
+      );
+
+      this.matrixQuad2Rect = inv(this.matrixRect2Quad);
+      this.inputs.forEach((i) => {
+        // If an input is generic, this will be undefined since there is no class
+        if (i) i.update(this);
+      });
+    }
+  }
+}
+
+export default InputGroup;
