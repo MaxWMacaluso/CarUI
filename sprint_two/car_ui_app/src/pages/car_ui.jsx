fc4cfa43f5fe7cb65cb790e9c65d732fbbfd546e
Peter Martin <peterjmartin2000@gmail.com>
2020-10-13 02:53:23 -0600

    Car Ui page loads from pages contingent on profile id.

--- sprint_two/car_ui_app/src/pages/car_ui.jsx
+++ sprint_two/car_ui_app/src/pages/car_ui.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import { Link, useLocation, BrowserRouter as Router } from "react-router-dom";
 import CarUIMoveable from '../components/CarUIMoveable'; //'./' is current folder
 import FileUpload from '../components/FileUpload';
+import { useParams } from "react-router";
 
 // var value = "target"
 //
@@ -19,22 +20,61 @@ import FileUpload from '../components/FileUpload';
 
 
 function CarUIPage () {
+  // Variables for Moveable
   const [moveableComponentReference, setMoveableCommponentReference] = useState(React.createRef());
   const [begin, setBegin] = useState(null);
   const [end, setEnd] =  useState(null);
 
+  const [imgs, setImgs] = useState(false);
+  const location = useLocation();
+  let { profile_id } = useParams();
+  const [tick, setTick] = useState(false);
 
+  useEffect(() => {
+    if (tick == 0) {
+      getImg();
+    }
+    setTick(1)
+  });
 
+  function getImg() {
+    console.log(location);
+    // var profile_id = location.search.query.profile_id;
+    // console.l
+    console.log(`http://localhost:3001/images-by-profile${location.search}`)
+        fetch(`http://localhost:3001/images-by-profile${location.search}`)
+          .then(response => {
+            return response.text();
+          })
+          .then(data => {
+            console.log("Data Retrieved:")
+            console.log(data);
+            var newImageLines = <div></div>
+            for (var row of data) {
+              var className = `moveable_${row.img_id}`;
+              newImageLines += <img id = "placedImage" className={className} src = {row.img_source} style = ""/>
+            }
+            data = JSON.parse(data)
+            // data =[{"name":"test1"},{"name":"test2"}];
+            console.log(typeof data);
+            setImgs(data.map((d) => <div><li key={d.img_source}>{d.img_source}</li><img/></div>));
 
+            // setImgs(<div><div dangerouslySetInnerHTML={{__html: newImageLines}} ></div><h2>Here</h2></div>);
+          });
+  }
 
-  // changeClass(){
-  //   this.reloadChild();
-  //   console.log("hi")
-  //   if (this.value == "target")
-  //       this.value = "temp"
-  //     else
-  //       this.value = "target"
-  // }
+  function FormatImages() {
+    if (!imgs) {
+      return <h1> Help </h1>
+    }
+    var newImageLines = ''
+    for (var row of imgs) {
+      newImageLines += `<img id = "placedImage" className="moveable_${row.img_id}" src = "${row.img_source}" style = ""/>`
+    }
+    return <div><div dangerouslySetInnerHTML={{__html: newImageLines}} ></div><h2>Here</h2></div>
+;
+  }
+
   function changeTarget(newTarget, delay = 0) {
     if (newTarget == null || newTarget == "" || !newTarget.startsWith('moveable')){
       newTarget ="none"
@@ -46,18 +86,8 @@ function CarUIPage () {
     else {
       console.log('resettingstates')
       moveableComponentReference.current.resetStates(newTarget);
-
-      // this.moveableComponentReference.current.resetStates(newTarget);
-
     }
-
   }
-
-
-  useEffect(() => {
-    console.log(moveableComponentReference.current)
-    // this.setState({ bodyHeight: this.calendarBodyRef .current.clientHeight });
-  });
 
   function handleClick(e){
     console.log("mouse is down!")
@@ -73,14 +103,11 @@ function CarUIPage () {
     }
   }
 
-//             <img className="moveable_koala1" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Leo.svg/360px-Leo.svg.png"/>
-            // <img className="moveable_koala2" src="../../App_icon_4.svg"/>
-
-
     return (
         <div onMouseDown = {handleClick} onClick = {finishClick}>
         <div id = "canvas"></div>
             <CarUIMoveable id = "carUIMoveable" ref = {moveableComponentReference} moveableTarget="target" />
+            {imgs ? imgs : "No iamges here!"}
             <button onClick = {console.log('hi')}>click </button>
             <img id = "placedImage" className="moveable_koala3" src="https://www.treehugger.com/thmb/pzsLSvqKfyLxIvqIogiWba54u3c=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2019__05__koala-0f87652acc244db2ba7d2e231c868f16.jpg"/>
             <img id = "placedImage" className="moveable_koala4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Cutest_Koala.jpg/1117px-Cutest_Koala.jpg"/>
